ee4821338334c818dd6fae71277dea3b1ff8ecfb
Todor-Stefanov <Tistefanov@students.softuni.bg>
2022-08-26 21:50:33 +0300

    Adding Integer Operations Task to Exercises

---
+++ 02. Fundamentals/02. Data Types and Variables/Exercises/01. Integer Operations/01. Integer_Operations.py
@@ -0,0 +1,11 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+
+first_operation = a + b
+second_operation = int(first_operation / c)
+third_operation = second_operation * d
+
+print(third_operation)
