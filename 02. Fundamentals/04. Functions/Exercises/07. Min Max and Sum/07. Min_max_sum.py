5c6af325ec25727fe91cfbf9d2adfe8229b1440b
Todor-Stefanov <Tistefanov@students.softuni.bg>
2022-08-31 21:44:57 +0300

    Adding 07. Min Max and Sum task

---
+++ 02. Fundamentals/04. Functions/Exercises/07. Min Max and Sum/07. Min_max_sum.py
@@ -0,0 +1,7 @@
+def min_max_sum(num):
+    print(f"The minimum number is {min(num)}")
+    print(f"The maximum number is {max(num)}")
+    print(f"The sum number is: {sum(num)}")
+
+
+numbers = list(map(int, input().split(" ")))
